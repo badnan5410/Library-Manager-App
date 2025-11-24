181442b13ed8d74e191501257eaf63a4b5202a4d
Ali Ansari <badnan541@gmail.com>
2025-11-24 13:50:41 +0000

    Updated Admin.java so now it includes an admin email and admin password. It just makes the whole thing feel more authentic.

--- src/main/Admin.java
+++ src/main/Admin.java
@@ -6,15 +6,15 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Admin {
-    String adminName;
-    int adminID;
+    static String adminName;
+    static int adminID;
     String adminType;
+    static String adminEmail;
+    static String adminPassword;
 
     static Scanner scanner = new Scanner(System.in);
 
     public Admin() {
-
-        helper.Helper h = new Helper();
 
         do {
             Helper.delayTime(750);
@@ -24,15 +24,31 @@ public class Admin {
         } while (!adminType.equals("1") && !adminType.equals("2"));
 
         if (adminType.equals("1")) {
-            Helper.delayTime(750);
-            System.out.println("\n\nWelcome new admin!");
-            adminName = getAdminName();
-            adminID = getAdminId();
+            setupNewAdmin();
 
-            Helper.delayTime(750);
-            System.out.printf("\n\nName: %s", adminName);
-            System.out.printf("\nID: %d", adminID);
+        } else {
+            //TODO
         }
+    }
+
+    private static void setupNewAdmin() {
+        Helper.delayTime(750);
+        adminName = getAdminName();
+        Helper.delayTime(750);
+        adminEmail = getAdminEmail();
+        Helper.delayTime(750);
+        adminPassword = getAdminPassword();
+        Helper.delayTime(750);
+        adminID = getAdminId();
+
+        Helper.delayTime(750);
+        System.out.printf("\n\nWelcome %s!", adminName);
+        Helper.delayTime(750);
+        System.out.printf("\n\nPlease use your email address [%s] as your username when logging in.", adminEmail);
+        Helper.delayTime(750);
+        System.out.printf("\n\nLog in using your password [%s], make sure to write it down somewhere so you don't forget it!", adminPassword);
+        Helper.delayTime(750);
+        System.out.printf("\n\nHere is your unique admin ID [%d].", adminID);
     }
 
     private static String getAdminName() {
@@ -50,6 +66,28 @@ public class Admin {
         Random rand = new Random();
 
         return 10000000 + rand.nextInt(90000000);
+    }
+
+    private static String getAdminEmail() {
+        String adminEmail;
+
+        do {
+            System.out.print("\n\nPlease enter a valid email address: ");
+            adminEmail = scanner.nextLine();
+        } while (!adminEmail.contains("@") || !adminEmail.contains(".") || adminEmail.startsWith("@") || adminEmail.endsWith("@"));
+
+        return adminEmail;
+    }
+
+    private static String getAdminPassword() {
+        String adminPassword;
+
+        do {
+            System.out.print("\n\nPlease create a password (make sure it is greater than 7 characters and it includes numbers and special characters): ");
+            adminPassword = scanner.nextLine();
+        } while (adminPassword.length() < 8 || !adminPassword.matches(".*\\d.*") || !adminPassword.matches(".*[^a-zA-Z0-9].*"));
+
+        return adminPassword;
     }
 
 }
