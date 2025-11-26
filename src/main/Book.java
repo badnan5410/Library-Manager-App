78cbfa9eddaec6283e5ef9b7d5d28a4e7833497a
Ali Ansari <badnan541@gmail.com>
2025-11-26 12:31:45 +0000

    Created Book.java, and made the different fields for a book.

---
+++ src/main/Book.java
@@ -0,0 +1,14 @@
+package main;
+
+import java.util.List;
+
+public class Book {
+    int id;
+    String title;
+    String author;
+    List<String> genre;
+    int quantity;
+    int totalQuantity;
+
+    public Book() {}
+}
